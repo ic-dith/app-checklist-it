5b850d43c76d05601656149f441cbcfb88a43a31
ic-dith <ivan.confalonieri@dith.com>
2026-06-09 14:14:33 +0200

    .

--- src/app_config_xml_raw.ts
+++
@@ -1,5 +0,0 @@
-export const APP_CONFIG_XML_RAW = `<?xml version="1.0" encoding="UTF-8"?>
-<config>
-  <title>ClearTask Compliance</title>
-  <subtitle>Operational Safety Checklist Template & Audit</subtitle>
-</config>`;
